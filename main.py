afbfbbfac1c47589676ef01021e8c96ea980fd2e
ReieMatza <Reie.matza@vayyar.com>
2024-02-17 10:51:36 +0200

    something is working

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 from torch import nn
 from torch.utils.data import Dataset
@@ -7,49 +9,65 @@ from torchtext import datasets
 from torchtext.data import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torcheval.metrics.text import Perplexity
+import torch.nn.functional as F
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+SEQUENCE_SIZE = 20
 EMBEDDING_DIM = 6
-HIDDEN_DIM=200
+HIDDEN_DIM = 200
 BATCH_SIZE = 1
 LEARNING_RATE = 0.001
-SAVE_PATH = r".\model.pt"
+MODEL_SAVE_PATH = r".\model.pt"
+VOCAB_SAVE_PATH = r".\vocab.pt"
 train_file = r'C:\Users\ReieMatza\PycharmProjects\RNN\dataset\ptb.train.txt'
 valid_file = r'C:\Users\ReieMatza\PycharmProjects\RNN\dataset\ptb.valis.txt'
 test_file = r"C:\Users\ReieMatza\PycharmProjects\RNN\dataset\ptb.test.txt"
 
 
 class Ex2_dataset(torch.utils.data.Dataset):
-    def __init__(self, path_to_file):
+    def __init__(self, path_to_file, vocab_path=None):
         self.path = path_to_file
-        self.data, self.vocab = self.build_tokens_vocab()
+        self.data, self.vocab = self.build_tokens_vocab(vocab_path)
 
-    def build_tokens_vocab(self):
+    def build_tokens_vocab(self, vocab_path):
         tokenizer = get_tokenizer('basic_english')
+        self.max_len = 0
 
         # Build the vocabulary
-        def build_vocab(file_path):
+        def load_data(file_path):
             data = []
             with open(file_path, 'r', encoding='utf-8') as f:
                 Lines = f.readlines()
                 for l in Lines:
-                    data.append(tokenizer(l))
-
+                    tokens = tokenizer(l)
+                    self.max_len = len(tokens) if len(tokens) > self.max_len else self.max_len
+                    data.append(tokens)
             return data
 
-        train_tokens = build_vocab(self.path)
-        vocab = build_vocab_from_iterator(train_tokens, specials=["<unk>"])
-        return train_tokens, vocab
+        data = load_data(self.path)
+        if vocab_path is None:
+            vocab = build_vocab_from_iterator(data, specials=["<eos>", "<unk>"])
+            torch.save(vocab, VOCAB_SAVE_PATH)
+        else:
+            vocab = torch.load(VOCAB_SAVE_PATH)
+
+        return data, vocab
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        return torch.as_tensor(self.vocab(self.data[index][:-1])), torch.as_tensor(self.vocab([self.data[index][-1]]))
+        x = torch.as_tensor(self.vocab(self.data[index]))
+        y = x[1:]
+        x = F.pad(x, (0, self.max_len - len(x)), 'constant', self.vocab(["<eos>"])[0])
+        y = F.pad(y, (0, self.max_len - len(y)), 'constant', self.vocab(["<eos>"])[0])
+        return x,y
 
 
 class LSTMReie(nn.Module):
-    def __init__(self, vocab_size: int , embedding_dim: int = EMBEDDING_DIM, hidden_dim: int = HIDDEN_DIM , dropout: bool = False):
+    def __init__(self, vocab_size: int, embedding_dim: int = EMBEDDING_DIM, hidden_dim: int = HIDDEN_DIM,
+                 dropout: bool = False):
         super(LSTMReie, self).__init__()
         self.hidden_dim = hidden_dim
         self.word_embedding = nn.Embedding(vocab_size, EMBEDDING_DIM)
@@ -64,29 +82,35 @@ class LSTMReie(nn.Module):
         return logits, state
 
     def init_state(self):
-        return torch.zeros( BATCH_SIZE, self.hidden_dim), torch.zeros(BATCH_SIZE, self.hidden_dim)
+        return torch.zeros(BATCH_SIZE, self.hidden_dim), torch.zeros(BATCH_SIZE, self.hidden_dim)
 
 
 def train(model: LSTMReie, train_data):
     train_loader = DataLoader(train_data, BATCH_SIZE)
     num_of_sequences = len(train_data)
+    loss = nn.CrossEntropyLoss()
     model.train()
-    loss_fun = Perplexity()
+    preplexity = Perplexity()
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     for i, (x, y) in enumerate(train_loader):
-        if x.nelement()>1:
-            state_hi, state_ci = model.init_state()
-            yi, (state_hi, state_ci) = model(torch.squeeze(x), (state_hi, state_ci))
-
-            loss = loss_fun.update(torch.unsqueeze(yi,0),x)
-            prediction = torch.argmax(yi[-1])
-            loss.compute()
+        x.to(device)
+        y.to(device)
+        if x.nelement() > 1:
+            yi, (state_hi, state_ci) = model(x)
+            cost = loss(torch.squeeze(yi),torch.squeeze(y))
+            cost.backward()
             optimizer.step()
-            if i % 100 == 0:
-                print(f" sequence {i} out of {num_of_sequences}, {i*100/num_of_sequences}%")
-    torch.save(model, SAVE_PATH)
+
+            # preplexity.update(yi,y)
+            # prediction = torch.argmax(yi[-1])
+            # preplexity{preplexity.compute()}
+            if i % 1000 == 0:
+                print(
+                    f" sequence {i} out of {num_of_sequences}, {i * 100 / num_of_sequences}%")
+    torch.save(model, MODEL_SAVE_PATH)
 
 
-train_data = Ex2_dataset(train_file)
+vocab_path = VOCAB_SAVE_PATH if os.path.isfile(VOCAB_SAVE_PATH) else None
+train_data = Ex2_dataset(train_file, vocab_path)
 
-train(LSTMReie(len(train_data.vocab)), train_data)
+train(LSTMReie(len(train_data.vocab)).to(device), train_data)
